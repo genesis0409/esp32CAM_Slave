fc4395ffa29363482aa69443af01668f630308f2
genesis0409 <smcdidcksthf@naver.com>
2024-01-22 02:15:21 +0900

    no comment

--- src/main.cpp
+++ src/main.cpp
@@ -39,24 +39,23 @@ void configDeviceAP();
 #include <addons/TokenHelper.h>
 
 // Replace with your network credentials
-const char *ssid = "REPLACE_WITH_YOUR_SSID";
-const char *password = "REPLACE_WITH_YOUR_PASSWORD";
+// const char *ssid = "REPLACE_WITH_YOUR_SSID";
+// const char *password = "REPLACE_WITH_YOUR_PASSWORD";
 
 // Insert Firebase project API Key
-#define API_KEY "AIzaSyBVjlrZ4uVERHfE5H9c2UoZvrbNE9YyWqs"
+// #define API_KEY "REPLACE_WITH_YOUR_FIREBASE_PROJECT_API_KEY."
 
 // Insert Authorized Email and Corresponding Password - OR add "userCredential.h"
-// #define USER_EMAIL ""
-// #define USER_PASSWORD ""
+// #define USER_EMAIL "REPLACE_WITH_THE_AUTHORIZED_USER_EMAIL"
+// #define USER_PASSWORD "REPLACE_WITH_THE_AUTHORIZED_USER_PASSWORD"
 
 // Insert Firebase storage bucket ID e.g bucket-name.appspot.com
-#define STORAGE_BUCKET_ID "REPLACE_WITH_YOUR_STORAGE_BUCKET_ID"
 // For example:
 // #define STORAGE_BUCKET_ID "esp-iot-app.appspot.com"
 
-// Photo File Name to save in LittleFS
+// Photo File Name to save in LittleFS/SPIFFS
 #define FILE_PHOTO_PATH "/photo.jpg"
-#define BUCKET_PHOTO "/data/photo.jpg"
+#define BUCKET_PHOTO_PATH "/data"
 
 void setup()
 {
@@ -97,7 +96,7 @@ void loop()
     //     gfx->fillScreen(BLACK);
     //     unsigned long start = millis();
     //     jpegClass.draw(&SPIFFS,
-    //                    (char *)"/moon.jpg", jpegDrawCallback, true /* useBigEndian */,
+    //                    (char *)"FILE_PHOTO_PATH", jpegDrawCallback, true /* useBigEndian */,
     //                    0 /* x */, 0 /* y */, gfx->width() /* widthLimit */, gfx->height() /* heightLimit */);
 
     //     Serial.printf("Time used: %lu\n", millis() - start);
@@ -114,18 +113,20 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
     // 2:
     switch (*data++) // *data++ == *(data++); 어떻게 해석해야될지 모르겠단말이지: *data(첫 번째 원소)로 switch넣고 나서 ++한다 -> 두 번째 원소 가리키도록 함
     {
+    case 0x00: // camID 등 송신자 정보 출력 용도, 케이스 메시지를 나눠야겠는데 CAMID 수만큼 ㄷㄷ
+        Serial.println();
     case 0x01: // startTransmit() 전송 시작을 알리는 메시지: 전송 데이터량 고지
         Serial.println("Start of new file transmit");
         currentTransmitCurrentPosition = 0;                    // 전송위치 초기화
         currentTransmitTotalPackages = (*data++) << 8 | *data; // 데이터 재구축: 2번째 원소(2nd 8bits)를 Left Shift 하여 비트 OR로 3번재 원소(1st bits)와 합침 -> 패키지 수 표현하는 하위 16비트 재구축)
         Serial.println("currentTransmitTotalPackages = " + String(currentTransmitTotalPackages));
-        SPIFFS.remove("/moon.jpg"); // 이전 파일 삭제; 이름은 자유
+        SPIFFS.remove("FILE_PHOTO_PATH"); // 이전 파일 삭제; 이름은 자유
         break;
     case 0x02: // sendNextPackage() 다음 패키지 전송을 위한 기능
         // Serial.println("chunk of file transmit");
         currentTransmitCurrentPosition = (*data++) << 8 | *data++; // 데이터 재구축: 같은 원리로 [현재 전송 위치] 복원
         // Serial.println("chunk NUMBER = " + String(currentTransmitCurrentPosition));
-        File file = SPIFFS.open("/moon.jpg", FILE_APPEND); // 파일을 덮어쓰지 않고 이어붙이도록 open
+        File file = SPIFFS.open("FILE_PHOTO_PATH", FILE_APPEND); // 파일을 덮어쓰지 않고 이어붙이도록 open
         if (!file)
             Serial.println("Error opening file ...");
 
@@ -141,7 +142,7 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len)
         {
             // showImage = 1; // 삭제 예정 - 이미지 표시기능
             Serial.println("Done File Transfer");
-            File file = SPIFFS.open("/moon.jpg");
+            File file = SPIFFS.open("FILE_PHOTO_PATH");
             Serial.println(file.size());
             file.close();
 
